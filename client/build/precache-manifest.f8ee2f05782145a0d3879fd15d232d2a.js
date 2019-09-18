d8df103e7da9aba3fd4246d8a7f3ecd568746e6c
raxemmohamed <raxemmohamed@gmail.com>
2019-09-18 11:34:02 -0500

    commit

---
+++ client/build/precache-manifest.f8ee2f05782145a0d3879fd15d232d2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ccef3dbf2748d55f36cb6ed039a9908",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ceaed7360730f9ca9a7c",
+    "url": "/static/css/main.7162ef6b.chunk.css"
+  },
+  {
+    "revision": "e7345aa6f9009728fc00",
+    "url": "/static/js/2.8b1dfb5c.chunk.js"
+  },
+  {
+    "revision": "ceaed7360730f9ca9a7c",
+    "url": "/static/js/main.2e344824.chunk.js"
+  },
+  {
+    "revision": "2e59372fcbd87fe0891e",
+    "url": "/static/js/runtime~main.cfaf67bf.js"
+  }
+]);
